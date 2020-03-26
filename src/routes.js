d01dbaaf05c0f87b3102828c7389b2d5ded11a81
johnnyloreano <laureano.pedrojoao@gmail.com>
2020-03-26 17:57:50 -0300

    backend completed!

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const ongController = require('./controllers/OngController');
+const incidentController = require('./controllers/IncidentController');
+const profileController = require('./controllers/ProfileController');
+const sessionController = require('./controllers/SessionController');
+
+const connection = require('./database/connection');
+
+const routes = express.Router();
+
+routes.get('/ongs/', ongController.index);
+routes.post('/ongs/', ongController.create);
+
+routes.get('/profile', profileController.index);
+
+routes.get('/session', sessionController.validate);
+
+routes.get('/incidents/', incidentController.index);
+routes.post('/incidents/', incidentController.create);
+routes.delete('/incidents/:id', incidentController.delete);
+
+module.exports = routes;
