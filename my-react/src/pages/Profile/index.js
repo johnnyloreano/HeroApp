43b5647fdb38378c53e3dfa40d5dce7a974cba99
johnnyloreano <laureano.pedrojoao@gmail.com>
2020-03-28 19:14:10 -0300

    added profile and cases

--- my-react/src/pages/Profile/index.js
+++ my-react/src/pages/Profile/index.js
@@ -1,11 +1,55 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import logoImg from '../../assets/logo.svg';
+import { FiPower,FiTrash2 } from 'react-icons/fi'
+import './styles.css';
 
-
-export default function(){
+export default function Profile(){
     return(
-        <h1>
-            Teste
-        </h1>
+        <div className="profile-container">
+            <header>
+                <img src={logoImg} alt = "Be the hero" />
+                <span>Bem vindo, APAD</span>
+
+                <Link className = "button" to = "/incidents/new">Cadastrar novo Caso</Link>
+                <button type= "button" >
+                    <FiPower size = {18} color = "#E02041" />
+                </button>
+            </header>
+            <h1>Casos cadastrados :</h1>
+            <ul>
+                <li>
+                    <strong> CASO: </strong>
+                    <p> Caso teste</p>
+
+                    <strong> DESCRIÇÃO: </strong>
+                    <p> Descrição teste</p>
+
+                    <strong> VALOR: </strong>
+                    <p>R$ 120,00</p>
+                    
+                    <button type="button">
+                        <FiTrash2 size = {20} color = "#a8a8b3" />
+                    </button>
+                </li>
+                
+                <li>
+                    <strong> CASO: </strong>
+                    <p> Caso teste</p>
+
+                    <strong> DESCRIÇÃO: </strong>
+                    <p> Descrição teste</p>
+
+                    <strong> VALOR: </strong>
+                    <p>R$ 120,00</p>
+                    
+                    <button type="button">
+                        <FiTrash2 size = {20} color = "#a8a8b3" />
+                    </button>
+                </li>
+
+            </ul>
+        </div>
 
     )
 }
--- my-react/src/routes.js
+++ my-react/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Logon from './pages/Logon';
 import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 import Register from './pages/Register';
 
 export default function Routes(){
@@ -12,6 +13,8 @@ export default function Routes(){
                 <Route path = "/profile" component={Profile} />
                 <Route path = "/cadastro" component={Register} />
                 <Route path = "/" exact component={Logon} />
+                <Route path = "/incidents/new" exact component={NewIncident} />
+                
             </Switch>
         </BrowserRouter>
 
